6e582d554888fcadd76ab096b8d0f7e09fa0a0bc
Admin <admin@Admins-MacBook-Pro.local>
2023-09-19 10:31:01 +0700

    initial commit

---
+++ mybookstore/models.py
@@ -0,0 +1,11 @@
+# models.py
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
