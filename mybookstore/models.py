d5d7c90242a9bbadd0dcb1dfe461c71f0d93c390
jeabcodemad <mail2jeab@gmail.com>
2023-09-23 18:04:31 +0700

    author,book models registed for django admin

--- mybookstore/models.py
+++ mybookstore/models.py
@@ -1,11 +1,16 @@
-# models.py
 from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     publication_date = models.DateField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
